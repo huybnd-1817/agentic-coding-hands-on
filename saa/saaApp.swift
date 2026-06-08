d5df043224ab582a03f460ea7e59da86f8ea8ff9
nguyen.duc.huyb <nguyen.duc.huy-b@sun-asterisk.com>
2026-06-08 16:12:08 +0700

    Initial Commit

---
+++ saa/saaApp.swift
@@ -0,0 +1,17 @@
+//
+//  saaApp.swift
+//  saa
+//
+//  Created by nguyen.duc.huyb on 8/6/26.
+//
+
+import SwiftUI
+
+@main
+struct saaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ saaTests/saaTests.swift
@@ -0,0 +1,17 @@
+//
+//  saaTests.swift
+//  saaTests
+//
+//  Created by nguyen.duc.huyb on 8/6/26.
+//
+
+import Testing
+@testable import saa
+
+struct saaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
